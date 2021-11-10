b233560d90734f9c577acae06ff687b5d0327794
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-11-10 22:22:23 +0000

    typo install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 -m venv venv
-. venv/binactivate
+. venv/bin/activate
 pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio===0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
 
 pip install transformers==4.12.3
