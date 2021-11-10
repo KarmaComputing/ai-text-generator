2d62f3427f918f67236006638afae5d9f626e8a4
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-11-10 22:19:17 +0000

    ai text generation gpt neo

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python3 -m venv venv
+. venv/binactivate
+pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio===0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+
+pip install transformers==4.12.3
+pip install Flask==2.0.2
---
+++ main.py
@@ -0,0 +1,25 @@
+from transformers import pipeline
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+generator = pipeline("text-generation", model="EleutherAI/gpt-neo-2.7B")
+
+
+@app.route("/")
+def hello_world():
+    resp = """<p>Post to / for generated text.
+            e.g. curl -d prompt='What is the meaning of life?' http://127.0.0.1:5000/generate
+            </p>
+            """
+    return resp
+
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    prompt = request.form.get("prompt", None)
+    max_length = request.form.get("max_length", 50)
+    breakpoint()
+    res = generator(prompt, max_length=max_length, do_sample=True, temperature=0.9)
+    return res
